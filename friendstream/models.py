9fe74051e2f81e6d94063848155ffcc44117a9ed
Mark Paschal <markpasc@markpasc.org>
2011-02-15 19:17:57 -0800

    Order Account's properties differently (and let Video.embed_code be blank for now)

--- friendstream/models.py
+++ friendstream/models.py
@@ -5,11 +5,13 @@ from django.db import models
 
 class Account(models.Model):
 
-    user = models.ForeignKey('auth.User', blank=True)
     service = models.CharField(max_length=30)
     ident = models.CharField(max_length=255)
-    authinfo = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255)
+    # TODO: permalink_url and userpic?
+
+    user = models.ForeignKey('auth.User', blank=True, null=True)
+    authinfo = models.CharField(max_length=255, blank=True)
     last_updated = models.DateTimeField(default=datetime(2001, 1, 1))
     last_success = models.DateTimeField(default=datetime.now)
 
@@ -21,7 +23,7 @@ class Video(models.Model):
 
     width = models.IntegerField(blank=True, null=True)
     height = models.IntegerField(blank=True, null=True)
-    embed_code = models.TextField()
+    embed_code = models.TextField(blank=True)
 
 
 class UserStream(models.Model):
