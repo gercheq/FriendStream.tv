5edaab90f99f1ac7ff09db5dbf2903406ddfe10d
Mark Paschal <markpasc@markpasc.org>
2011-02-15 19:16:53 -0800

    Make the admin have some nice lists

--- friendstream/admin.py
+++ friendstream/admin.py
@@ -3,6 +3,25 @@ from django.contrib import admin
 from friendstream.models import *
 
 
-admin.site.register(Account)
-admin.site.register(Video)
-admin.site.register(UserStream)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('display_name', 'service', 'ident', 'user')
+
+admin.site.register(Account, AccountAdmin)
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('service', 'ident')
+
+admin.site.register(Video, VideoAdmin)
+
+
+class UserStreamAdmin(admin.ModelAdmin):
+    list_display = ('user', 'video_ident', 'poster_ident', 'posted')
+
+    def video_ident(self, meh):
+        return ':'.join((meh.video.service, meh.video.ident))
+
+    def poster_ident(self, meh):
+        return u'%s (%s:%s)' % (meh.poster.display_name, meh.poster.service, meh.poster.ident)
+
+admin.site.register(UserStream, UserStreamAdmin)
