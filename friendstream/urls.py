bf990e8bd108c31191009485cff171742d067489
Mark Paschal <markpasc@markpasc.org>
2011-02-17 22:03:06 -0800

    Display videos in the web page

--- friendstream/urls.py
+++ friendstream/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('friendstream.views',
     url(r'^$', 'home', name='home'),
-    url(r'^videos$', 'videos'),
+    url(r'^videos$', 'videos', name='videos'),
 
     url(r'^signin/twitter$', 'signin_twitter', name='signin-twitter'),
     url(r'^complete/twitter$', 'complete_twitter', name='complete-twitter'),
