7a17fadeb78929dad3206329ab98f866213952b8
Mark Paschal <markpasc@markpasc.org>
2011-03-04 09:48:48 -0800

    Use django-social-auth for auth

--- friendstream/urls.py
+++ friendstream/urls.py
@@ -4,7 +4,4 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('friendstream.views',
     url(r'^$', 'home', name='home'),
     url(r'^videos$', 'videos', name='videos'),
-
-    url(r'^signin/twitter$', 'signin_twitter', name='signin-twitter'),
-    url(r'^complete/twitter$', 'complete_twitter', name='complete-twitter'),
 )
