2675f9ff64ad497a2cbf08a00ce56f0efe43c94f
Mark Paschal <markpasc@markpasc.org>
2011-03-06 12:57:17 -0800

    Try to save all this extra data from Twitter

--- friendstream/models.py
+++ friendstream/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import logging
 
 from django.db import models
+import oauth2 as oauth
 from social_auth.signals import pre_update
 from social_auth.backends.twitter import TwitterBackend
 
@@ -10,24 +12,34 @@ class Account(models.Model):
     service = models.CharField(max_length=30)
     ident = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255)
-    permalink_url = models.CharField(max_length=255)
-    avatar_url = models.CharField(max_length=255)
+    permalink_url = models.CharField(max_length=255, blank=True)
+    avatar_url = models.CharField(max_length=255, blank=True)
 
-    user = models.ForeignKey('auth.User', blank=True, null=True)
+    user = models.ForeignKey('auth.User', blank=True, null=True, unique=True)
     authinfo = models.CharField(max_length=255, blank=True)
     last_updated = models.DateTimeField(default=datetime(2001, 1, 1))
     last_success = models.DateTimeField(default=datetime.now)
 
 
 def update_twitter_account(sender, user, response, details, **kwargs):
-    logging.getLogger(__name__).debug('Making twitter account with response %r and details %r and kwargs %r', response, details, kwargs)
-    return True
-
+    ident = response['id']
+    log = logging.getLogger(__name__)
+    log.debug('Making twitter account with ident %r', ident)
     try:
         account = Account.objects.get(user=user)
     except Account.DoesNotExist:
-        account, created = Account.objects.get_or_create(service='twitter.com', ident='moo')
-    user.gender = response.get('gender')
+        account, created = Account.objects.get_or_create(service='twitter.com', ident=ident)
+        account.user = user
+
+    screen_name = details['username']
+    account.display_name = details.get('fullname', screen_name)
+    account.permalink_url = 'http://twitter.com/%s' % screen_name
+    account.avatar_url = response.get('profile_image_url', '')
+
+    token = oauth.Token.from_string(response['access_token'])
+    account.authinfo = ':'.join((token.key, token.secret))
+
+    account.save()
     return True
 
 pre_update.connect(update_twitter_account, sender=TwitterBackend)
