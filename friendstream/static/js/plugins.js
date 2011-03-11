9479b69a2dc457b1f91299dc9935b6f3a92afae6
gercheq <gercekk@gmail.com>
2011-03-10 18:06:34 -0700

    replaced plugins.js with the new version

--- friendstream/static/js/plugins.js
+++ friendstream/static/js/plugins.js
@@ -2,20 +2,66 @@
 ** FriendStream.TV User Interactions
 **
 ** Author:    Gercek Karakus
-** Date:      March 04, 2011
+** Date:      March 08, 2011
 **
 */
 
 
-/*
-** STREAM PANEL
-*/
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return email.match(re)
+}
+
+function submit_form(){
+
+  $("#facebox #submit-email").attr('disabled','disabled');
+
+  $.ajax({
+     type: "POST",
+     url: "/save_email",
+     data: {
+       email: $('#facebox #input-email').attr('value')
+     },
+     success: function(msg){
+       $('#facebox #modal-contact-form .inner').html('<h3>Thank You!</h3><p>We will send you an invite as soon as possible.</p>');
+       init_cufon();
+     }
+  });
+}
+
+function check_errors(){
+  if( validateEmail($('#facebox #input-email').attr('value')) ){
+    $('#facebox .error-msg').fadeOut();
+    $("#facebox #input-email").removeClass('error');
+  }
+}
+
+function display_errors(){
+  // display errors
+  if( !$("#facebox #input-email").hasClass('error') ){
+    $("#facebox #input-email").addClass('error').after('<p class="error-msg">Please enter a valid email address.</p>');
+  }
+
+  // check if the errors are fixed
+  $("#facebox #input-email").keyup(function(){
+    check_errors();
+  });
+
+  $("#facebox #input-email").blur(function(){
+    check_errors();
+  });
+}
+
+
+/**************************************************************
+** Setup Stream Panel
+***************************************************************/
 function setup_stream_panel() {
 
   var $stream_container = $('#stream-container');
   $stream_container.animation_speed = '300';
   $stream_container.width_large = $stream_container.width();
-  $stream_container.width_small = '40px';
+  $stream_container.width_small = '30px';
 
   var $wrapper = $('#wrapper');
 
@@ -26,6 +72,7 @@ function setup_stream_panel() {
 
     // Close the stream panel if it is already open
     if( $('#stream-container').hasClass('active') ) {
+
       $stream_container.removeClass('active');
       $stream_container.animate({
           width: $stream_container.width_large
@@ -52,46 +99,87 @@ function setup_stream_panel() {
 
   });
 
-  // Stream Panel On HOVER
-  $s_items.hover(function(){
-    $(this).addClass('s-hover');
-  }, function(){
-    $(this).removeClass('s-hover');
-  });
-
-}
-
-/*
-** SETUP MODALS
-*/
-function setup_private_beta_modals() {
-	$("#yoxview-3").yoxview({
-    allowInternalLinks: true,
-    onOpen: function(){
-
-      // Update Fonts in the Modal
-      init_cufon();
-
-      $("#submit-email").click( function(){
-        if( validateEmail($('#input-email').attr('value')) ){
-          submit_form();
-        } else {
-          display_errors();
-        }
-        return false;
-      });
+  /*
+  ** Stream Panel On HOVER
+  */
+  $s_items.live('mouseover mouseout', function(event) {
+    if (event.type == 'mouseover') {
+      $(this).addClass('s-hover');
+    } else {
+      $(this).removeClass('s-hover');
     }
   });
 
-  // Trigger sign up model if user clicks on the tv and the logo as well
-  $('#yoxview-1, #yoxview-2').click(function(){
-    $('#yoxview-3').click();
+  /*
+  ** Stream Item On CLICK
+  */
+  $s_items.live('click',function(event){
+    $this = $(this);
+    $video_container = $('#video-container');
+
+    // Add .selected to currently clicked element
+    $('.selected', $stream_container).removeClass('selected');
+    $this.addClass('selected');
+
+    // Update the embed code
+    var video_id = $this.find('.siv-id').html();
+    var video_provider = $this.find('.siv-provider').html();
+    var embed_code = generate_embed_code(video_provider, video_id);
+    $video_container.find('#video-embed').html(embed_code);
+
+    // Update user details
+    var video_title = $this.find('.siv-title').html();
+    var video_service = $this.find('.friend-provider').html().split('.',1);
+    var friend_avatar = $this.find('.friend-avatar').html();
+    var friend_name = $this.find('.friend-name').html();
+    var friend_message = $this.find('.friend-message').html();
+
+    $video_container.find('h3').html(video_title);
+    // WTF - The code below is not working.
+    // video_service is assigned correctly but
+    // class is not getting added to the p element :/
+    // alert(video_service);
+    // $video_container.find('.metadata').addClass(video_service);
+    $video_container.find('.avatar').html(friend_avatar);
+    $video_container.find('blockquote').html(friend_message);
+    $video_container.find('.friend-name').html(friend_name);
+
   });
 }
 
-/*
-** LAYOUT MANAGER
-*/
+function show_first_video() {
+  $('.s-item').eq(0).click();
+}
+
+
+/**************************************************************
+** Setup Modals
+***************************************************************/
+function setup_private_beta_modals() {
+
+  $('a[rel*=facebox]').facebox({
+    loadingImage : 'css/images/loading.gif',
+    closeImage   : 'css/images/closelabel.png'
+  });
+
+  $(document).bind('reveal.facebox', function() {
+    init_cufon();
+
+    $("#facebox #submit-email").live('click', function(){
+      if( validateEmail($('#facebox #input-email').attr('value')) ){
+        submit_form();
+      } else {
+        display_errors();
+      }
+      return false;
+    });
+  });
+
+}
+
+/**************************************************************
+** Layout Manager
+***************************************************************/
 function calculate_dimensions(){
   var $window = $(window);
   var $video_container = $('#video-container');
@@ -99,10 +187,9 @@ function calculate_dimensions(){
   var $stream = $('#stream');
   var $navigation = $('#navigation');
   var $logo = $('#logo');
+  var $video_embed_shadow = $('#video-embed-shadow');
   var $video_embed = $('#video-embed');
   var $video_details = $('#video-details');
-
-
 
   // Setup #video-container
   var vc_margin_horizontal = $video_container.outerWidth(true) - $video_container.outerWidth();
@@ -115,13 +202,15 @@ function calculate_dimensions(){
   });
 
   // Setup #video-embed
-  var video_embed_height = $video_container.outerHeight() - $video_details.outerHeight(true) - vc_margin_vertical;
+  var video_embed_height = $video_container.outerHeight() - $video_details.outerHeight(true);
   $video_embed.css({
     'height' : video_embed_height
   });
 
-
-
+  //Setup #video-embed-shadow
+  $video_embed_shadow.css({
+    'height' : video_embed_height
+  });
 
   // Setup #stream-container
   $stream_container.css({
@@ -137,19 +226,159 @@ function calculate_dimensions(){
     'height' : stream_height
   });
 
+}
 
 
+/**************************************************************
+** Text Replacement - Cufon
+***************************************************************/
+function init_cufon(){
+  Cufon.replace('#home h3, .button, footer a', { fontFamily: 'Gotham Rounded Medium' });
+	Cufon.replace('#home p', { fontFamily: 'Gotham Rounded Light' });
+	Cufon.replace('.cufon', { fontFamily: 'Gotham Rounded Medium' });
+}
+
+
+/**************************************************************
+** Setup Video Stream
+***************************************************************/
+/*
+    {
+        "poster": {
+            "ident": "22915745",
+            "display_name": "Kenneth Kufluk",
+            "service": "twitter.com",
+            "avatar_url": "http://a3.twimg.com/profile_images/1224766800/i_really_need_a_haircut_shave_newshirt_normal.jpg"
+        },
+        "message": "Current status: http://t.co/ltgYjyB (NSFW)",
+        "video": {
+            "ident": "RJC8pJ7xbB0",
+            "service": "youtube.com"
+        },
+        "id": 30,
+        "posted": "2011-03-09T00:45:47"
+    }
+*/
+var video_json;
+
+function load_videos(json_url){
+
+  $.getJSON(json_url, function(json_data) {
+    video_json = json_data;
+    var items = [];
+    $.each(json_data, function(key, val) {
+
+      // clean up twitter avatar_url
+      if (json_data[key].poster.service == "twitter.com"){
+        var avatar_url_small = json_data[key].poster.avatar_url;
+        var avatar_url = avatar_url_small.replace("_normal", "_reasonably_small");
+      } else {
+        var avatar_url = json_data[key].poster.avatar_url;
+      }
+
+      var video ='<div class="s-item clearfix ' + json_data[key].poster.service.split('.',1) + '" id="video-'+ json_data[key].id + '">\
+                    <div class="si-thumb"><img src="css/images/video_thumb_default.jpg"/></div>\
+                    <div class="si-info">\
+                      <div class="si-video">\
+                        <h3 class="siv-title">Video Title</h3>\
+                        <div class="siv-desc hidden">Video Description</div>\
+                        <div class="siv-id hidden">' + json_data[key].video.ident + '</div>\
+                        <div class="siv-provider hidden">' + json_data[key].video.service + '</div>\
+                      </div>\
+                      <div class="si-user">\
+                        <span class="icon"></span> \
+                        <span class="friend-name ' + json_data[key].poster.ident + '">' + json_data[key].poster.display_name + '</span>\
+                        <div class="hidden friend-avatar">\
+                          <img src="' +  avatar_url + '">\
+                        </div>\
+                        <div class="hidden friend-provider">' + json_data[key].poster.service + '</div>\
+                        <div class="hidden friend-message">' +  json_data[key].message + '</div>\
+                      </div>\
+                    </div>\
+                  </div>';
+      items.push(video);
+    });
+
+    var tmp = items.join('');
+    $('#stream').html(tmp);
+
+    // Get video details for each video in the stream from APIs
+    $('.s-item').each(function(){
+      get_video_details($(this));
+    });
+
+    // Show the first video
+    show_first_video();
+
+  });
+}
+
+/*
+** Generate embed code from video_provider name and video_id
+** Embed code is going to be included in the #video-container
+*/
+function generate_embed_code(video_provider, video_id){
+
+  var embed_code = "";
+
+  if (video_provider == "youtube.com") {
+    embed_code = '<iframe title="YouTube video player" width="100%" height="100%" src="http://www.youtube.com/embed/' + video_id + '" frameborder="0" allowfullscreen></iframe>';
+  } else if (video_provider == "vimeo.com") {
+    embed_code = '<iframe src="http://player.vimeo.com/video/' + video_id + '?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1"  width="100%" height="100%" frameborder="0"></iframe>';
+  }
+
+  return embed_code;
 }
 
 
 /*
-** Text Replacement - Cufon
+** Get video details such as thumbnail, title, description
+** from respective APIs. Currently supporting only Youtube and Vimeo
 */
-function init_cufon(){
-  // Cufon.replace('h3, .button, footer a', { fontFamily: 'Gotham Rounded Medium' });
-	// Cufon.replace('p', { fontFamily: 'Gotham Rounded Light' });
-	Cufon.replace('.cufon', { fontFamily: 'Gotham Rounded Light' });
+function get_video_details($stream_video) {
+
+  var video_id = $stream_video.find('.siv-id').html();
+  var video_provider = $stream_video.find('.siv-provider').html();
+
+  if (video_provider == "youtube.com"){
+    var url = "http://gdata.youtube.com/feeds/api/videos/" + video_id + "?v=2&alt=json"
+    $.get(url, function(data) {
+        $stream_video.find('h3').html( data.entry.title.$t );
+        $stream_video.find('.siv-desc').html( data.entry.media$group.media$description.$t );
+        $stream_video.find('.si-thumb img').attr('src', data.entry.media$group.media$thumbnail[0].url );
+    });
+  } else if (video_provider == "vimeo.com") {
+    var url =  "http://vimeo.com/api/v2/video/" + video_id + ".json?callback=?";
+
+    $.getJSON(url, {format: "json"}, function(data) {
+        $stream_video.find('h3').html( data[0].title);
+        $stream_video.find('.siv-desc').html( data[0].description );
+        $stream_video.find('.si-thumb img').attr('src', data[0].thumbnail_small);
+    });
+  }
 }
+
+
+/*
+** DEPRECATED
+**
+function get_thumbnail(video_provider, video_id, size)
+{
+  if (video_id === null){ return ""; }
+
+  size = (size === null) ? "small" : size;
+
+  if (video_provider == "youtube.com") {
+    if(size == "small"){
+      return "http://img.youtube.com/vi/" + video_id + "/2.jpg";
+    } else {
+      return "http://img.youtube.com/vi/" + video_id + "/0.jpg";
+    }
+  } else if (video_provider == "vimeo.com") {
+    return "no vimeo support"
+  }
+}
+*/
 
 
 /**************************************************************
@@ -157,13 +386,16 @@ function init_cufon(){
 ***************************************************************/
 $(document).ready(function(){
   init_cufon();
+
+  // App
   setup_stream_panel();
-  setup_private_beta_modals();
   calculate_dimensions();
+  load_videos('videos.json');
+
+  // Home
+  setup_private_beta_modals();
 
 });
-
-
 
 /**************************************************************
 ** ON WINDOW RESIZE
