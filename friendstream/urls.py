45b02c7b1cf501144a6202583c2eb020b01ccacc
Mark Paschal <markpasc@markpasc.org>
2011-03-10 18:18:32 -0800

    Be where the javascript expects

--- friendstream/urls.py
+++ friendstream/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('friendstream.views',
     url(r'^$', 'home', name='home'),
-    url(r'^videos$', 'videos', name='videos'),
+    url(r'^videos\.json$', 'videos', name='videos'),
     url(r'^save_email$', 'save_email', name='save_email'),
 )
