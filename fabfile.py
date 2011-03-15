fca6d89b2257c55097d1f66efe9f19fcc49da4d0
Mark Paschal <markpasc@markpasc.org>
2011-03-15 12:53:56 -0700

    Discover identity key from ssh config (so keys work for more than just rsync)

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,11 @@
+from os.path import expanduser
+
 from fabric.api import run, cd, env
-
 from fabric.contrib.project import rsync_project
+from paramiko.config import SSHConfig
 
 
-env.hosts = ['gercheq@friendstream.tv']
+env.hosts = ['friendstream.tv']
 
 ROOT_DIR = '/home/gercheq/webapps/django'
 
@@ -35,3 +37,27 @@ def restart():
     migrate_db()
     restart_apache()
     restart_worker()
+
+
+def annotate_hosts_with_ssh_config_info():
+    def hostinfo(host, config):
+        hive = config.lookup(host)
+        if 'user' in hive:
+            host = '%s@%s' % (hive['user'], host)
+        if 'port' in hive:
+            host = '%s:%s' % (host, hive['port'])
+        return host
+
+    try:
+        config_file = file(expanduser('~/.ssh/config'))
+    except IOError:
+        pass
+    else:
+        config = SSHConfig()
+        config.parse(config_file)
+        keys = [expanduser(config.lookup(host).get('identityfile', None))
+            for host in env.hosts]
+        env.key_filename = [key for key in keys if key is not None]
+        env.hosts = [hostinfo(host, config) for host in env.hosts]
+
+annotate_hosts_with_ssh_config_info()
